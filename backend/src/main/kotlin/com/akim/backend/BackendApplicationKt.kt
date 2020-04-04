eeec576b0a4cdbccbe69f8f7ca4a3ecde70a0b67
Lebedev Konstantin Alekseevich <k.a.lebedev@tinkoff.ru>
2020-04-05 00:34:41 +0300

    .master init

---
+++ backend/src/main/kotlin/com/akim/backend/BackendApplicationKt.kt
@@ -0,0 +1,13 @@
+package com.akim.backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackendApplicationKt
+
+    fun main(args: Array<String>) {
+        runApplication<BackendApplicationKt>(*args)
+    }
+
+
