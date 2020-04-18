c06a849bca404f8bc9c43b4f87322f221b6c827d
tsekh <tsekh11@gmail.com>
2020-04-18 19:33:25 +0300

    add new sidebar

--- frontend/src/containers/Header/index.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const MyHeader = () => {return <div></div>};
-
-export default MyHeader;
--- frontend/src/containers/HomePage/index.js
+++ frontend/src/containers/HomePage/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import Sidebar from "../Sidebar";
 import Content from "../Content";
 import Box from "@material-ui/core/Box";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
 const HomePage = () => {
     return <Box display="flex">
