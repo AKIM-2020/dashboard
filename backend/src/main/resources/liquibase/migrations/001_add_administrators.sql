597a647b869364b1bbc0b64c2d83089583270220
slavich <slavichmo@gmail.com>
2020-05-12 16:43:52 +0300

    Remove migration

--- backend/src/main/resources/liquibase/migrations/001_add_administrators.sql
+++
@@ -1,6 +0,0 @@
-insert into super_admins (id, first_name, last_name, city, balance, user_id, currency) values
-    (nextval('super_admins_seq'), 'andrew', 'mac', 'newyork', 100, 1, 'EUR'),
-    (nextval('super_admins_seq'), 'andrew', 'mac', 'newyork', 100, 2, 'EUR'),
-    (nextval('super_admins_seq'), 'andrew', 'mac', 'newyork', 100, 3, 'EUR'),
-    (nextval('super_admins_seq'), 'andrew', 'mac', 'newyork', 100, 4, 'EUR'),
-    (nextval('super_admins_seq'), 'andrew', 'mac', 'newyork', 100, 1, 'EUR');
